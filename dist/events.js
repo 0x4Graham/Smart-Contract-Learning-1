95385487c5cb75be4a566876c67da9f9ec9cf2f3
Grey007 <grahamnelson@live.com>
2018-07-02 19:09:19 +0200

    more

--- dist/events.js
+++ dist/events.js
@@ -1,27 +1,5 @@
 
 
-function checkDriverStatus(val) {
-    var driverstatus;
-
-    switch (val) {
-        case 0:
-            driverstatus = "Requested";
-            break;
-        case 1:
-            driverstatus = "Registered";
-            break;
-        case 2:
-            driverstatus = "NotRegistered";
-            break;
-        case 3:
-            driverstatus = "Failed";
-            break;
-        case 4:
-            driverstatus = "Pending";
-            break;
-    }
-    return driverstatus;
-}
 
 
 
